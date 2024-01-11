b01806482260ebf90d1268bc46e1579c59bf9b61
dkanders12 <dkanders12@gmail.com>
2024-01-11 17:21:50 +0100

    Footer done

--- Leo-Love/src/components/Footer/Footer.jsx
+++ Leo-Love/src/components/Footer/Footer.jsx
@@ -1,9 +1,43 @@
-
+import "./Footer.scss";
+import footerImage from "../../assets/icons/Vector-1-image.png";
+import footerImage1 from "../../assets/icons/Vector-image.png";
 export const Footer = () => {
-    return(
-<footer>
-    Footer element
-</footer>    
-    )
-}
+  return (
+    <footer>
+      <div>
+        <h3>Adresse</h3>
+        <p>
+          Find os her:
+          <br />
+          <br />
+          Vestervænget 232, 30.sal 6574 Øster Nørup Danmark
+        </p>
+      </div>
+      <div>
+        <h3>Kontakt</h3>
+        <p>
+          Kontakt os her:
+          <br />
+          <br />
+          email@mail.dk Tlf: 0192 3023
+        </p>
+      </div>
+      <div>
+        <h3>Politik</h3>
 
+        <p>
+          Vores politikker:
+          <br />
+          <br /> Privatlivspolitk Cookiepolitik Generalle betingelse
+        </p>
+      </div>
+      <div>
+        <h3>Sociale medier</h3>
+        <div className="imgContainer">
+          <img src={footerImage} alt="Footer Image" />
+          <img src={footerImage1} alt="Footer Image" />
+        </div>
+      </div>
+    </footer>
+  );
+};
