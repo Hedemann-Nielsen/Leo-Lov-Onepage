5a83dd10743994eebf3e6aa3ee4f619785632a09
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-01-11 12:35:09 +0100

    issue #3

--- Leo-Love/src/components/navbar/navbar.jsx
+++ Leo-Love/src/components/navbar/navbar.jsx
@@ -1,23 +1,23 @@
 import style from './Navbar.module.scss';
 
-export const Navbar = props => {
-    return (
-        <>
-        <div className={style.navBar}>
-           <ul className={style.left}>
-                {props.data && props.data.map((item, itemIndex) => {
-                    return <li key={itemIndex}>{item}</li>
-                }
-                
-                )}
-            </ul> 
-            <div className={style.right}>
-                <h3>
-                    <span>Leo</span>-Love
-                </h3>
-            </div>
+export const Navbar = (props) => {
+  return (
+    <>
+      <div className={style.navBar}>
+        <ul className={style.left}>
+          {props.data &&
+            props.data.map((item, itemIndex) => (
+              <li key={itemIndex}>
+                <a href={`#${item.toLowerCase()}`}>{item}</a>
+              </li>
+            ))}
+        </ul>
+        <div className={style.right}>
+          <h3>
+            <span>Leo</span>-Love
+          </h3>
         </div>
-        </>
-      
-    )
-  }
+      </div>
+    </>
+  );
+};
