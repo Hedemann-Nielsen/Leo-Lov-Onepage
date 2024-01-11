a0d7da3d6e01c9c7d6a4242183915d4f27803cb0
Hedemann-Nielsen <127857522+Hedemann-Nielsen@users.noreply.github.com>
2024-01-11 09:27:59 +0100

    issue #1

--- Leo-Love/src/App.jsx
+++ Leo-Love/src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
+import { Fallback } from './pages/Fallback/Fallback'
+import { Home } from './pages/Home/Home'
 import './App.scss'
 
 function App() {
 
   return (
     <>
-
+<Routes>
+  <Route index element={<Home />} />
+  <Route path="*" element={<Fallback />} />
+</Routes>
     </>
   )
 }
